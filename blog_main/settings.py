75e1550082d65ed1596416e48d4151cdc0054d45
Subathees <dev16.smakintel@gmail.com>
2024-03-11 22:05:38 +0530

    registration and login

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     
     'blogs',
     'assignments',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
